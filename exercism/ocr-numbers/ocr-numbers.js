11247c5d34710e82c60806f8aa1d1f917178e683
Kamila Lambert <kamila.p.lambert@gmail.com>
2017-03-28 17:51:25 -0700

    ocr convert passing zero function

---
+++ exercism/ocr-numbers/ocr-numbers.js
@@ -0,0 +1,14 @@
+var ocr = function() {};
+
+ocr.convert = function(number) {
+  if (number == ' _ \n' + '| |\n' + '|_|\n' + '   ') {
+    return '0'
+  }
+
+};
+
+
+module.exports = ocr;
+
+
+// check if it is a 3x4 grid
